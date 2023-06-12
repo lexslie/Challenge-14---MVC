5d3ddb9fe170c2468d98e619d7b82251cacb20fc
lexslie <lesliiee727@gmail.com>
2023-06-12 13:43:23 -0700

    update user-routes.js
    added code to create a new user

--- Develop/controllers/api/user-routes.js
+++ Develop/controllers/api/user-routes.js
@@ -46,3 +46,69 @@ router.get("/:id", (req, res) => {
         res.status(500).json(err);
     });
 });
+
+
+// create user
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {username: req.body.username},
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({
+                message: "There is no existing user with the chosen username!."
+            });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_is = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({
+                user: dbUserData,
+                message: "You've successfully logged in!"
+            });
+        });
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword) {
+            res.status(400).json({
+                message: "Incorrect password."
+            });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({
+                user: dbUserData,
+                message: "You've successfully logged in!"
+            });
+        });
+    });
+});
