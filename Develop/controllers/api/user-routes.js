ac7c71a2b0de0f550b10c0469ef17e0937ed8250
lexslie <lesliiee727@gmail.com>
2023-06-12 13:32:09 -0700

    update user-routes.js
    added code to view an individual user

--- Develop/controllers/api/user-routes.js
+++ Develop/controllers/api/user-routes.js
@@ -12,3 +12,37 @@ router.get("/", (req, res) => {
         res.status(500).json(err);
     });
 });
+
+
+// get single user
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: {exclude: ["password"]},
+        where: {id: req.params.id},
+        include: [{
+            model: Post,
+            attributes: ["id", "title", "content", "created_at"]
+        },
+        {
+            model: Comment,
+            attributes: ["id", "comment_text", "created_at"],
+            include: {
+                model: Post,
+                attributes: ["title"]
+            }
+        }]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({
+                message: "There was no user found with this id."
+            });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
