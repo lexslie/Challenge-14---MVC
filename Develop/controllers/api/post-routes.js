50ed23b8a02c9848987ab410228b503e4336f4fe
lexslie <lesliiee727@gmail.com>
2023-06-12 13:10:19 -0700

    update post-routes.js
    added code to view all posts and individual posts, and to create a post

---
+++ Develop/controllers/api/post-routes.js
@@ -0,0 +1,81 @@
+const router = require("express").Router();
+const {Post, User, Comment} = require("../../models");
+const withAuth = require("../../utils/auth");
+
+
+// get+find all posts
+router.get("/", (req, res) => {
+    Post.findAll({
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        order: [
+            ["created_at", "DESC"]
+        ],
+        include: [{
+            model: User,
+            attributes: ["username"],
+        },
+        {
+            model: Comment,
+            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+            include: {
+                model: User,
+                attributes: ["username"]
+            },
+        }],
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// get+find single post
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: {id: req.params.id},
+        attributes: ["id", "title", "content", "created_at"],
+        include: [{
+            model: User,
+            attributes: ["username",]
+        }
+        {
+            model: Comment,
+            attributes: ["id", "title", "content", "created_at"],
+            include: {
+                model: User,
+                attributes: ["username"],
+            },
+        }],
+    })
+    .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({
+                message: "There was no post found with this id."
+            });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+
+// create post
+router.post("/", withAuth, (req, res) => {
+    console.log("Creating post");
+    Post.create({
+        title: req.body.title,
+        content: req.body.post_content,
+        user_id: req.session.user_id
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
