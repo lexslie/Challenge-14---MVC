9cbae720f5254fb46236b321cf3d054ed72b0fdf
lexslie <lesliiee727@gmail.com>
2023-06-12 11:48:40 -0700

    update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -10,4 +10,34 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
     secret: process.env.DB_SECRET,
-}
+    cookie: {},
+    resave: false, 
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        // to check every 10 min & expire after 30 min
+        checkExpirationInterval: 1000 * 60 * 10,
+        expiration: 1000 * 60 * 30
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(routes);
+
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}!`);
+});
