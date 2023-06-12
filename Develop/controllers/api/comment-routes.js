99c9d7f7c71377d0a80742d9d2b03fe297a3ab32
lexslie <lesliiee727@gmail.com>
2023-06-12 13:49:26 -0700

    update comment-routes
    added code to view all comments

--- Develop/controllers/api/comment-routes.js
+++ Develop/controllers/api/comment-routes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
 const {User, Post, Comment} = require("../../models");
 const withAuth = require("../../utils/auth");
+
+
+// get all comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
